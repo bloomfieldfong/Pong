1fe30bd46245a6539762c396555ce8860daceb93
bloomfieldfong <bloomfieldf24@gmail.com>
2017-03-12 17:56:26 -0600

    pong

---
+++ Pong2/Assets/Codigo/Jugadores.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Jugadores : MonoBehaviour {
+	
+	Vector2 porteria;
+	public int player;
+	float   factorVel;
+	bool x; 
+
+	
+	// Use this for initialization
+	void Start () {
+		porteria = new Vector2(0.0f,0.0f);
+		factorVel = 10.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		porteria.y = 0.0f;
+	
+		if (player ==1){		
+			if (Input.GetKey(KeyCode.UpArrow) ){
+				porteria.y = factorVel * Time.deltaTime;
+				transform.Translate(porteria);	
+			}
+			
+			if (Input.GetKey(KeyCode.DownArrow) ){
+				porteria.y = -factorVel * Time.deltaTime;
+				transform.Translate( porteria);	
+			}
+				
+		}
+		
+		if (player == 2){		
+			if (Input.GetKey(KeyCode.W) ){
+				porteria.y = factorVel * Time.deltaTime;
+				transform.Translate( porteria);	
+			}
+			
+			if (Input.GetKey(KeyCode.S) ){
+				porteria.y = -factorVel * Time.deltaTime;
+				transform.Translate(porteria);	
+			}
+			
+		}
+
+		if (Input.GetKey (KeyCode.R)) {
+			SceneManager.LoadScene (0); 
+		}
+		
+	}
+}
---
+++ Pong2/Assets/Codigo/Pelota.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Pelota : MonoBehaviour {
+
+	int fuerza = 65;
+
+
+	// Use this for initialization
+	void Start () {
+	//	GetComponent<Rigidbody2D> ().AddForce (transform.up * 250);
+		int a = Random.Range (0, 2);
+		if (a <= 1) {
+			GetComponent<Rigidbody2D> ().AddForce (transform.up * fuerza);
+			GetComponent<Rigidbody2D> ().AddForce (transform.right * fuerza);
+		} else {
+			GetComponent<Rigidbody2D> ().AddForce (-transform.up * fuerza);
+			GetComponent<Rigidbody2D> ().AddForce (-transform.right * fuerza);
+		}
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	//Cuando toque las paredes se destruira la bola
+	void OnCollisionEnter2D(Collision2D col){
+		
+		if(col.gameObject.name == "Izquierda"){
+			Destroy (this.gameObject);
+		}
+		
+		if(col.gameObject.name == "Derecha"){
+			Destroy(this.gameObject);
+		
+		}
+	}
+
+}
